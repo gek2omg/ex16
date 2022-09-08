bece1837536e05d483707f0d6be39d3d047ebd77
gek2omg <gek2omg@gmail.com>
2022-09-08 12:16:31 +0900

    Initial commit

---
+++ src/main/java/com/example/ex16/Ex16Application.java
@@ -0,0 +1,13 @@
+package com.example.ex16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex16Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex16Application.class, args);
+    }
+
+}
